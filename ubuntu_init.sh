8142577bae767cc914b7d7ea5983555366ba6cc3
mdubois <xd.dubois@gmail.com>
2024-04-14 15:31:45 +0000

    Create ubuntu_init.sh

---
+++ ubuntu_init.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Ubunut install
+ sudo apt install gnome-shell-extensions
+ sudo apt install gnome-shell-extension-manager
+  # blur my shell
+  # dash to Panel
+  # ArcMenu
+  # User theme
+ sudo snap install discord \
+ 	code \
+ 	keepassxc \
+ 	spotify
+
+wget https://proton.me/download/mail/linux/ProtonMail-desktop-beta.deb
+sudo apt install ./ProtonMail-desktop-beta.deb
+
+sudo add-apt-repository ppa:linuxuprising/guake
+sudo apt-get update
+sudo apt install guake
+# Tomorrow night blue
+
+# Docker
+# https://docs.docker.com/engine/install/ubuntu/#install-using-the-repository
